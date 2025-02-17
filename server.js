a99eb2c15203049f817d0d9b8863492f124cd2ba
komaldesigner <58035201+developpitamaas@users.noreply.github.com>
2025-02-17 13:04:52 +0530

    check and update it

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/category", categoryRoutes);
 app.use('/api/cart', cartRoutes);
 // app.use('/api/orders', orderRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
