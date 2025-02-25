e89d3ef24b6562030782486c6b526b4e4b50bff5
Pitamaas <developpitamaas@gmail.com>
2025-02-25 06:21:57 +0000

    mallana-replace the files new files

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/orders', orderRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/banners", bannerRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8002;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
