adafa42be80efe757d44b872835c455bbe42374b
Pitamaas <developpitamaas@gmail.com>
2025-02-27 05:33:39 +0000

    new change

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use("/api/baat-cheet", baatCheetRoutes);
 app.use("/api/humko-join-karlo", humkoJoinKarloRoutes);
 app.use("/api/shipping", shippingRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8002;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
